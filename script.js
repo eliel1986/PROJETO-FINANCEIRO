c72da498940465ec9201cdcd1e3ec76d46a8093e
eliel1986 <eliel_1986@hotmail.com>
2024-05-01 21:04:05 -0300

    Mensagens de erro e refatoração

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-function validateFields() {
-    const emailValid = isEmailValid();
-    document.getElementById("recover-password-button").disabled = !emailValid;
+function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();   
+}
 
-    const passwordValid = isPasswordValid();
-    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
+function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
 }
 
 function isEmailValid() {
-    const email = document.getElementById("email").value;
+    const email = form.email().value;
     if (!email) {
         return false;
     }
     return validateEmail(email);
 }
 
+function toggleEmailErrors() {
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? 'none' : 'block';
+
+    form.emailInvalidError().style.display = validateEmail(email) ? 'none' : 'block';
+}
+
+function togglePasswordErrors() {
+    const password = form.password().value;
+
+    form.passwordRequiredError().style.display = password ? 'none' : 'block';
+}
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    form.recoverPassword().disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    form.loginButton().disabled = !emailValid || !passwordValid;
+
+}
+
 function isPasswordValid() {
-    const password = document.getElementById("password").value;
+    const password = form.password().value;
     if(!password) {
         return false;
     }
     return true;
 }
 
-function validateEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
+const form = {
+    email: () => document.getElementById('email'),
+    emailInvalidError: () => document.getElementById('email-invalid-error'),
+    emailRequiredError: () => document.getElementById('email-required-error'),
+    loginButton: () => document.getElementById('login-button'),
+    password: () => document.getElementById('password'),
+    passwordRequiredError: () => document.getElementById('password-required-error'),
+    recoverPassword: () => document.getElementById('recover-password-button')
 }
